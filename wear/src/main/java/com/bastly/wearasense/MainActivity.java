c0924c2b7a4010a4d6342844fe9c49ad0e9ca6e3
Alex Vidal <goofyahead@gmail.com>
2015-07-06 13:14:12 +0200

    initial commit

---
+++ wear/src/main/java/com/bastly/wearasense/MainActivity.java
@@ -0,0 +1,85 @@
+package com.bastly.wearasense;
+
+import android.app.Activity;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.os.Vibrator;
+import android.support.wearable.view.WatchViewStub;
+import android.util.Log;
+import android.view.WindowManager;
+import android.widget.TextView;
+
+import com.bastly.wearasense.Utils.VibrationManager;
+
+public class MainActivity extends Activity implements SensorEventListener {
+
+    private static final String TAG = MainActivity.class.getName();
+    private static long elapsed = 0;
+    private TextView mTextView;
+    private SensorManager mSensorManager;
+    Sensor accelerometer;
+    Sensor magnetometer;
+    Vibrator vibration;
+    float azimut;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        setContentView(R.layout.activity_main);
+
+        vibration = (Vibrator) this.getSystemService(Context.VIBRATOR_SERVICE);
+
+        final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
+        stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
+            @Override
+            public void onLayoutInflated(WatchViewStub stub) {
+                mTextView = (TextView) stub.findViewById(R.id.text);
+            }
+        });
+
+        mSensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+        accelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        magnetometer = mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+    }
+
+    protected void onResume() {
+        super.onResume();
+        mSensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_UI);
+        mSensorManager.registerListener(this, magnetometer, SensorManager.SENSOR_DELAY_UI);
+    }
+
+    protected void onPause() {
+        super.onPause();
+        mSensorManager.unregisterListener(this);
+    }
+
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {}
+
+    float[] mGravity;
+    float[] mGeomagnetic;
+    public void onSensorChanged(SensorEvent event) {
+        if (System.currentTimeMillis() - elapsed > 200) {
+            elapsed = System.currentTimeMillis();
+            if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER)
+                mGravity = event.values;
+            if (event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD)
+                mGeomagnetic = event.values;
+            if (mGravity != null && mGeomagnetic != null) {
+                float R[] = new float[9];
+                float I[] = new float[9];
+                boolean success = SensorManager.getRotationMatrix(R, I, mGravity, mGeomagnetic);
+                if (success) {
+                    float orientation[] = new float[3];
+                    SensorManager.getOrientation(R, orientation);
+                    azimut = orientation[0]; // orientation contains: azimut, pitch and roll
+                    Log.d(TAG, "orientation " + Math.toDegrees(azimut));
+                    vibration.vibrate(VibrationManager.getPattern(azimut), -1);
+                }
+            }
+        }
+    }
+}
---
+++ wear/src/main/java/com/bastly/wearasense/Utils/VibrationManager.java
@@ -0,0 +1,31 @@
+package com.bastly.wearasense.Utils;
+
+/**
+ * Created by goofyahead on 6/07/15.
+ */
+public class VibrationManager {
+
+    private static final int INTERVAL = 25;
+    private static final long[] none = {0, 0, 0, 0};
+    private static final long[] PATTERN_MINIMAL = {INTERVAL, INTERVAL, INTERVAL, 0, INTERVAL};
+    private static final long[] patternLow = {INTERVAL, INTERVAL, INTERVAL, INTERVAL};
+    private static final long[] patternMedium = {0, INTERVAL, 0, INTERVAL, INTERVAL, INTERVAL};
+    private static final long[] patternHigh = {0, INTERVAL, 0, INTERVAL, 0, INTERVAL, 0, INTERVAL};
+
+    public static long[] getPattern(float azimut) {
+        if (azimut > 0) { // half pointing north
+            if (azimut > -45 || azimut < -135) {
+                return PATTERN_MINIMAL;
+            } else if (azimut > -67 || azimut < -123) {
+                return patternLow;
+            } else if (azimut > -87 || azimut < -93) {
+                return patternMedium;
+            } else {
+                return patternHigh;
+            }
+        }
+        else {
+            return none;
+        }
+    }
+}
